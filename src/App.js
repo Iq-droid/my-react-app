9a7720ce7972ae4fc6a0763ddd5247df0f40ed68
iqra-ali <iqra.ali@student.moringaschool.com>
2023-04-23 23:15:19 +0300

    image&&text

--- src/App.js
+++ src/App.js
@@ -4,20 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="image-container">
+        <img src="https://images.unsplash.com/photo-1491466424936-e304919aada7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80"className="App-logo" alt="logo" />
+        <div className="image-description">
+          <p>Strolling down the canyon. It was midnight and auroras were bursting, but i was stuck in the canyon, so i tried to make as much use as i can from the position where i was. The idea fell on my mind to light the sides with the torches and another selfie came out :)</p>
+        </div>
+      </div>
     </div>
   );
 }
